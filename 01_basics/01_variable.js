11647b7115dc2384e55ece388f2eb88eaf937903
Abhishekcodermg <157596226+Abhishekcodermg@users.noreply.github.com>
2024-01-25 06:20:28 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = "00415607721"
+let accountEmail = "abhishek281020000@gmail.com"
+var accountpassword = "12345"
+acountcity = "Jaipur"
+
+
+
+console.table([accountId,accountEmail,accountpassword,acountcity])               
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// "use strict" ; //treat all js code as newer version
+
+// alert(3+3); //  this will not work because we are  using nodejs not browser
+
+console.log([3+3]);
+
+let name = "Abhishek";
+
+/* datatypes are
+
+    Number=> 2 to the power 53
+    BigInt => is for very very big Number
+    string => to be enclosed in doouble quotes ""
+    Boolean => true/false 
+    null = standalone value 
+    undefined 
+    symbol => to find uniqueness
+
+
+
+    */
+
+    console.log(typeof "Abhishek" );
